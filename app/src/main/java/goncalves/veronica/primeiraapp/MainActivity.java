39626a54166ced79b2850a5b88cae17d8ad04639
20221TIIMI0076 <ve.goncalvesdesouza@gmail.com>
2024-04-02 08:28:23 -0300

    término dos comentários

--- app/src/main/java/goncalves/veronica/primeiraapp/MainActivity.java
+++ app/src/main/java/goncalves/veronica/primeiraapp/MainActivity.java
@@ -19,11 +19,11 @@ public class MainActivity extends AppCompatActivity {   // Tela principal da Apl
         btnEnviar.setOnClickListener(new View.OnClickListener() { //define acao do botao ao clicar//
             @Override
             public void onClick(View v) { //exexuta a acao no clique do botao//
-                EditText etDigiteAqui= findViewById(R.id.etDigiteAqui);
-                String textoDigitado = etDigiteAqui.getText().toString();
-                Intent i = new Intent(MainActivity.this, NextActivity.class);
-                i.putExtra("texto", textoDigitado);
-                startActivity(i);
+                EditText etDigiteAqui= findViewById(R.id.etDigiteAqui);//obtem o elemento de campo de texto//
+                String textoDigitado = etDigiteAqui.getText().toString();//obtem o texto da caixa de texto//
+                Intent i = new Intent(MainActivity.this, NextActivity.class);//sinaliza intencao do sistema operacional//
+                i.putExtra("texto", textoDigitado);//coloca o texto digitado na intencao//
+                startActivity(i);//inicia a proxima activity baseada na intencao//
 
             }
         });
--- app/src/main/java/goncalves/veronica/primeiraapp/NextActivity.java
+++ app/src/main/java/goncalves/veronica/primeiraapp/NextActivity.java
@@ -12,10 +12,11 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Intent i = getIntent();
-        String textoDigitado = i.getStringExtra("texto");
+
+        Intent i = getIntent();//obtem a intencao de mainActivity//
+        String textoDigitado = i.getStringExtra("texto");//obtem o texto digitado da intencao//
         TextView tvTexto = findViewById(R.id.tvTexto);
-        tvTexto.setText(textoDigitado);
+        tvTexto.setText(textoDigitado);//muda o texto do next activity//
         startActivity(i);
     }
 }
